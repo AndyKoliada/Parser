56989dbc6279d43e7b717b72cf4b4d9705d15d79
Andrii Koliada <andykoliada@gmail.com>
2020-06-05 13:10:49 +0300

    Add project files.

---
+++ Parser/Program.cs
@@ -0,0 +1,32 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace Parser
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://dumskaya.net/user/neravnoduschnyj/";
+
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+
+            Console.WriteLine(html.Result);
+            Console.ReadLine();
+        }
+    }
+}
