cafb0a8f2b880672b23a821e139e0bc2076a3463
Andrii Koliada <andykoliada@gmail.com>
2020-06-07 10:00:19 +0300

    a

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
 using System.Web;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -12,51 +15,84 @@ namespace Parser
 {
     class Program
     {
-        static ITelegramBotClient botClient;
+        //static ITelegramBotClient botClient;
+        
 
         static void Main(string[] args)
         {
 
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Encoding encoding = Encoding.GetEncoding("windows-1251");
-            GetHtmlAsync();
 
-            //static ITelegramBotClient botClient;
+            System.Timers.Timer t = new System.Timers.Timer(5000);
+            t.AutoReset = true;
+            t.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            t.Start();
 
-
-            #region TELEGRAMBOT
-
-            botClient = new TelegramBotClient("1139402041:AAHbmIdDK1XaIXVhilFOgNF9uiAkHmj667c");
-
-            var me = botClient.GetMeAsync().Result;
-            Console.WriteLine(
-              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
-            );
-
-            botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving();
-            Thread.Sleep(int.MaxValue);
-
-
-            Console.ReadLine();
-        }
-
-                #endregion
-
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text != null)
+            async void OnTimedEvent(Object source, ElapsedEventArgs e)
             {
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
-
-                await botClient.SendTextMessageAsync(
-                  chatId: e.Message.Chat,
-                  text: "You said:\n" + e.Message.Text
-                );
+                await GetHtmlAsync();
+                await SendToBotAsync();
             }
+
+
+            //GetHtmlAsync();
+            //SendToBotAsync();
+
+
+            //#region TELEGRAMBOT
+
+            //botClient = new TelegramBotClient("1139402041:AAHbmIdDK1XaIXVhilFOgNF9uiAkHmj667c");
+
+            //var me = botClient.GetMeAsync().Result;
+            //Console.WriteLine(
+            //  $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            //);
+
+            //botClient.OnMessage += Bot_OnMessage;
+            //botClient.StartReceiving();
+            //Thread.Sleep(int.MaxValue);
+
         }
 
-        private static async void GetHtmlAsync()
+        private static async Task SendToBotAsync()
+        {
+
+            string recentMessage = "";
+
+
+            var bot = new TelegramBotClient("1139402041:AAHbmIdDK1XaIXVhilFOgNF9uiAkHmj667c");
+            var t = await bot.SendTextMessageAsync("@neravnodushny_bot", recentMessage);
+        }
+
+        //#endregion
+
+        //static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        //{
+        //    if (e.Message.Text != null)
+        //    {
+        //        Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+        //        await botClient.SendTextMessageAsync(
+        //          chatId: e.Message.Chat,
+        //          text: "You said:\n" + e.Message.Text
+        //        );
+        //    }
+        //}
+
+        //        Message message = await botClient.SendTextMessageAsync(
+        //  chatId: e.Message.Chat, // or a chat id: 123456789
+        //  text: "Trying *all the parameters* of `sendMessage` method",
+        //  parseMode: ParseMode.Markdown,
+        //  disableNotification: true,
+        //  replyToMessageId: e.Message.MessageId,
+        //  replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl(
+        //    "Check sendMessage method",
+        //    "https://core.telegram.org/bots/api#sendmessage"
+        //  ))
+        //);
+
+        public static async Task GetHtmlAsync()
         {
             string url = "https://dumskaya.net/user/neravnoduschnyj/";
 
@@ -90,13 +126,13 @@ namespace Parser
 
             //string m = HttpUtility.HtmlDecode(recentMessage.InnerText.TrimStart(':',' ').Replace(" &nbsp;", " "));
 
-
+           
            foreach(var i in rawMessagesList)
             {
                 //string x = HttpUtility.HtmlDecode(i.InnerText.TrimStart(':', ' ').Replace(" &nbsp;", " "));
                 if(i.InnerText.StartsWith(':'))
                 {
-                    string x = HttpUtility.HtmlDecode(i.InnerText.TrimStart(':', ' ').Replace(" &nbsp;", " ").Replace("&#133",""));
+                    string x = HttpUtility.HtmlDecode(i.InnerText.TrimStart(':', ' ').Replace(" &nbsp;", " ").Replace("&#133","").Replace(">", "\"").Replace("<", "\""));
                     Console.WriteLine(x);
                     Console.WriteLine();
                 }
@@ -115,18 +151,8 @@ namespace Parser
 
 
         #region TIMER
-        //public void Test()
-        //{
-        //    Timer t = new Timer(5000);
-        //    t.AutoReset = true;
-        //    t.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-        //    t.Start();
-        //}
 
-        //private async void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
 
-        //}
         #endregion
 
 
